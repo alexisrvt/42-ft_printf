7691e8d04eeaab668c552a1c4baad6d0f606a273
alexisrvt <114657997+alexisrvt@users.noreply.github.com>
2026-06-06 17:50:03 +0200

    Made dispatch function

--- dispatch_conversion.c
+++ dispatch_conversion.c
@@ -10,6 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h""
+#include "ft_printf.h"
 
 int	dispatch_conversion(t_format *fmt, va_list *ap)
+{
+    int printed_chars;
+    
+    printed_chars = 0;
+    if (fmt->type == 'c')
+        printed_chars = print_char(va_arg(*ap, int), fmt);
+    else if (fmt->type == 's')
+        printed_chars = print_string(va_arg(*ap, char *), fmt);
+    else if (fmt->type == 'd' || fmt->type == 'i')
+        printed_chars = print_int(va_arg(*ap, int), fmt);
+    else if (fmt->type == 'u')
+        printed_chars = print_unsigned(va_arg(*ap, unsigned int), fmt);
+    else if (fmt->type == 'x' || fmt->type == 'X')
+        printed_chars = print_hex(va_arg(*ap, unsigned int), fmt);
+    else if (fmt->type == 'p')
+        printed_chars = print_pointer(va_arg(*ap, void *), fmt);
+    else if (fmt->type == '%')
+        printed_chars = print_percent(fmt);
+    return (printed_chars);
+}  
+ 
