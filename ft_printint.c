dcc2eca73134655c4347d4143fb701d6452a60db
alexisrvt <114657997+alexisrvt@users.noreply.github.com>
2026-06-08 22:25:25 +0200

    Finishing ft_printint

--- ft_printint.c
+++ ft_printint.c
@@ -12,6 +12,21 @@
 
 #include "ft_printf.h"
 
+static int	ft_numlen(long n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	while (n > 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
 static int	ft_putabsnbr(long n)
 {
 	int		count;
@@ -29,20 +44,30 @@ int	ft_printint(int n, t_format *fmt)
 {
 	int		printed;
 	long	nb;
+	int		numlen;
+	int		numzero;
 
 	printed = 0;
 	nb = n;
-	if (nb >= 0)
+	if (nb < 0)
+		nb = -nb;
+	numlen = ft_numlen(nb);
+	if (nb == 0 && fmt->dot == 1 && fmt->precision == 0)
+		numlen = 0;
+	if (fmt->dot == 1 && fmt->precision > numlen)
+		numzero = fmt->precision - numlen;
+	if (n >= 0)
 	{
 		if (fmt->plus == 1)
 			printed += write(1, "+", 1);
 		else if (fmt->space == 1)
 			printed += write(1, " ", 1);
 	}
-	else if (nb < 0)
-	{
+	else
 		printed += write (1, "-", 1);
-		nb = -nb;
-	}
-	printed += ft_putabsnbr(nb);
+	if (numzero > 0)
+		printed += ft_printpad('0', numzero);
+	if (numlen > 0)
+		printed += ft_putabsnbr(nb);
+	return (printed);
 }
